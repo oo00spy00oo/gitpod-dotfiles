41b9d6bcd70cfded49c6e38fb65e64c538d82ca8
Spy Kab <ooo000spy000ooo@gmail.com>
2022-10-14 18:47:56 +0700

    Add yq

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 wget -O .editorconfig https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.editorconfig
 wget -O .gitpod/.gitpod-default.yml https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.gitpod-default.yml
+yq eval-all '. as $item ireduce ({}; . *+ $item)' .gitpod/.gitpod-*.yml | yq 'sort_keys(.)' > .gitpod.yml
 
 brew install fzf croc yq
-
