87b4fcdeba08ba8600ae64dd509db3b596e043e8
Spy Kab <ooo000spy000ooo@gmail.com>
2022-10-14 19:04:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-# Combine Gitpod configs
+# Editor Config
 wget -O .editorconfig https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.editorconfig
+
+# Combine Gitpod configs
+mkdir -p .gitpod/
 wget -O .gitpod/.gitpod-default.yml https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.gitpod-default.yml
 
 # Install packages via Homebrew
