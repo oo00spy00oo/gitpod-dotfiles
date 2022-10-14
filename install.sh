3e18da096b0bc1d49681d7760a5bf031c549b285
Spy Kab <ooo000spy000ooo@gmail.com>
2022-10-14 18:35:54 +0700

    Update config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ wget -O .editorconfig https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c16
 wget -O .gitpod/.gitpod-default.yml https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.gitpod-default.yml
 
 brew install fzf croc yq
+
