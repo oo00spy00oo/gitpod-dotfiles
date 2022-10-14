122b4b57be2e41c45dc31b3d2e5ee443f51aa1f6
Spy Kab <ooo000spy000ooo@gmail.com>
2022-10-14 18:48:49 +0700

    Add comment

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
+# Combine Gitpod configs
 wget -O .editorconfig https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.editorconfig
 wget -O .gitpod/.gitpod-default.yml https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.gitpod-default.yml
 yq eval-all '. as $item ireduce ({}; . *+ $item)' .gitpod/.gitpod-*.yml | yq 'sort_keys(.)' > .gitpod.yml
 
+# Install packages via Homebrew
 brew install fzf croc yq
