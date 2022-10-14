9bb695d07f75129d686e9cde70cd8d8c235ab6df
Spy Kab <ooo000spy000ooo@gmail.com>
2022-10-15 00:14:19 +0700

    Update setup.sh script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@
 wget -O .editorconfig https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.editorconfig
 
 # Combine Gitpod configs
-mkdir -p .gitpod/
-wget -O .gitpod/.gitpod-default.yml https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.gitpod-default.yml
+mkdir -p ${GITPOD_REPO_ROOT}/.gitpod/
+wget -O ${GITPOD_REPO_ROOT}/.gitpod/.gitpod-default.yml https://gist.githubusercontent.com/oo00spy00oo/e4df74b3c1674fa350445a74fab3d6bf/raw/.gitpod-default.yml
 
 # Install packages via Homebrew
 brew install fzf croc yq
